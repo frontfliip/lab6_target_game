3cd0965b1704c026f5efcadb8ea87caf2c7ebde1
ivannikolaichenko <ivnik.cat@gmail.com>
2021-11-04 19:26:39 +0200

    target_game.py generate_grid() function done

---
+++ target_game.py
@@ -0,0 +1,43 @@
+from typing import List
+import random
+
+def generate_grid() -> List[List[str]]:
+    """
+    Generates list of lists of letters - i.e. grid for the game.
+    e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
+    """
+    lst = ["A", "B", "C", "D", "E", "F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
+    output = [[], [], []]
+    for i in range(3):
+        for _ in range(3):
+            output[i].append(random.choice(lst))
+    return output
+print (generate_grid())
+def get_words(f: str, letters: List[str]) -> List[str]:
+    """
+    Reads the file f. Checks the words with rules and returns a list of words.
+    """
+    pass
+
+
+
+def get_user_words() -> List[str]:
+    """
+    Gets words from user input and returns a list with these words.
+    Usage: enter a word or press ctrl+d to finish.
+    """
+    pass
+
+
+def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+    """
+    (list, list, list) -> list
+
+    Checks user words with the rules and returns list of those words
+    that are not in dictionary.
+    """
+    pass
+
+
+def results():
+    pass
